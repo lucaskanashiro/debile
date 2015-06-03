6690fde5b343a52c09b7889a90317a9d9b1bf58e
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2015-06-02 22:38:16 -0300

    Add support for JSHint checker

---
+++ debile/slave/wrappers/jshint.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2015 Lucas Kanashiro <kanashiro.duarte@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from firehose.model import Issue, File, Message, Location, Point
+import re
+
+
+line_pattern_1 = '\t*<error line="(?P<line>\d+)" column="(?P<col>\d+)" '
+line_pattern_2 = 'severity="(?P<severity>[^"]+)" message="(?P<msg>[^"]+)" '
+line_pattern_3 = 'source="jshint.(?P<testid>[\w\d]+)"'
+
+FILE_RE = re.compile(r'\t*<file name="(?P<file>[^"]+)"')
+LINE_RE = re.compile(line_pattern_1 + line_pattern_2 + line_pattern_3)
+
+
+def parse_jshint(lines):
+    for line in lines:
+        if FILE_RE.match(line):
+            path = FILE_RE.match(line).groupdict()
+        if LINE_RE.match(line):
+            info = LINE_RE.match(line).groupdict()
+        else:
+            continue
+
+        yield Issue(cwe=None, testid=info['testid'],
+                    location=Location(
+                        file=File(path['file'], None),
+                        function=None,
+                        point=Point(int(info['line']), int(info['col']))),
+                    severity=info['severity'],
+                    message=Message(text=info['msg']), notes=None, trace=None)
